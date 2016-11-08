0e8f4c242756fe751f9cd11dbb7cff01ae8702ca
Luke Big <mrzolwix@gmail.com>
2016-11-08 20:25:54 +0100

    http get with ngResource

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -4,43 +4,37 @@ angular.module('blogDetail').
 	component('blogDetail', {
 		// template: "<div class=''><h1 class='new-class'>{{title}}</h1><button ng-click='someClickTest()'>Click me!</button>	</div>",
 		templateUrl: '/templates/blog-detail.html',
-		controller: function($http, $scope, $routeParams, $location) {
+		controller: function(Post, $http, $scope, $routeParams, $location) {
 
-			$http.get("/json/posts.json").then(successCallback, errorCallback)
+			// console.log(Post.get())
+			// console.log(Post.query())
+			//output is the same
 
-			function successCallback(response, status, config, statusText) {
-				var blogItems = response.data;
-				$scope.posts = blogItems;
-				angular.forEach(blogItems, function(post) {
+			Post.query(function(data) {
+				angular.forEach(data, function(post) {
 					if (post.id == $routeParams.id) {
-						console.log(post);
 						$scope.post = post;
 						$scope.notFound = false;
 					}
 				})
-			}
+			})
 
-			function errorCallback(response, status, config, statusText) {
-				$scope.notFound = true;
-			}
+			// $http.get("/json/posts.json").then(successCallback, errorCallback)
 
-			// var blogItems = [
-			// 	{title: "some title1", id: 1, description: "this is a book"},
-			// 	{title: "some title2", id: 2, description: "this is a book"},
-			// 	{title: "some title3", id: 3, description: "this is a book"},
-			// 	{title: "some title4", id: 4, description: "this is a book"},
-			// ]
+			// function successCallback(response, status, config, statusText) {
+			// 	var blogItems = response.data;
+			// 	$scope.posts = blogItems;
+			// 	angular.forEach(blogItems, function(post) {
+			// 		if (post.id == $routeParams.id) {
+			// 			$scope.post = post;
+			// 			$scope.notFound = false;
+			// 		}
+			// 	})
+			// }
 
-			// // console.log($routeParams)
-			// $scope.title = "Blog " + $routeParams.id;
-
-			// $scope.found = false;
-			// angular.forEach(blogItems, function(post){
-			// 	if (post.id == $routeParams.id) {
-			// 		$scope.post = post;
-			// 		$scope.found = true;
-			// 	}
-			// })
+			// function errorCallback(response, status, config, statusText) {
+			// 	$scope.notFound = true;
+			// }
 
 			if ($scope.notFound == true) {
 				//do sth f.e. set to 404
